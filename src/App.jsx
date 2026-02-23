730d520ec140c857d3455905f4fcd1c9cdf61c37
Michelle Gathoni <michellegathonimain@gmail.com>
2026-02-23 13:24:14 +0300

    Initial commit: Vite + React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import BusinessDashboard from './pages/BusinessDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>BUSINESS DASHBOARD</h1>
+      {/* <BusinessDashboard /> */}
+    </div>
+  );
+}
+
+export default App;
